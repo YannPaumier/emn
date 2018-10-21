3c9b058cef36a3eccbde8d9c365f4b84ff1fe9e7
yannpaumier <ypaumier@belacom.fr>
2018-10-21 19:42:33 +0200

    fixs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var xmlparser = require('express-xml-bodyparser');
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -16,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(xmlparser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,32 @@ router.get('/', function(req, res, next) {
 
 /* Sync */
 router.post('/auth', function(req, res, next) {
-  res.status(200).send("Response : " + req.body);
+
+  var request = require('request');
+  console.log(req.body);
+
+  var options = {
+    url: 'https://services.net-entreprises.fr/authentifier/1.0/',
+    method: 'POST',
+    headers: {
+      'User-Agent': 'Client-DSN (DsnBuilder/12.5; Paie.fr)',
+      'Content-Type' :'application/xml'
+    },
+    body: req.body
+  };
+ 
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //var info = JSON.parse(body);
+     res.status(200).send("Response : " + req.body);
+  }else{
+    res.status(400).send("Error : " + error);
+  }
+}
+ 
+request(options, callback);
+
+  
 });
 
 module.exports = router;
