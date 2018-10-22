f0f44882fc3e78e4af5f376f7804e62fdc054bb9
System Administrator <root@MacBook-Pro-de-Chantal.local>
2018-10-22 21:12:40 +0200

    ok send dns

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var http = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -38,81 +38,47 @@ router.post('/pushDsn', function(req, res, next) {
   var request = require('request');
   //var zip = require('lz-string');
   var zip = require('lzjs');
-  
+  var zlib = require('zlib');
+
   //console.log(req.body.jeton);
   //var compressed = zip.compress(req.body.file)
   var compressed = zip.compress(req.body.file)
   //console.log(compressed)
   //console.log(zip.decompress(compressed))
 
-
-  var req2 = http.request({
-    "host": "dsnrg.net-entreprises.fr",
-    "port": null,
-    "method": "POST",
-    "path": "/deposer-dsn/1.0/",
-    "headers": {
-      'Authorization': 'DSNLogin jeton='+req.body.jeton,
-      'User-Agent': 'Client-DSN (DsnBuilder/12.5; Paie.fr)',
-      'Content-Type' :'text/plain',
-      'Cache-Control': 'no-cache',
-      //'Content-Type' :'application/json',
-      'Content-Encoding' :'gzip',
-      'Accept-Encoding' :'gzip',
-      //'Accept-Encoding' :'gzip',
-      //'Content-Length' : 4096
-    },
-    }, function(res) {
-    var body = "";
-    res.on("data", function(chunk) {
-      body += chunk;
-      console.log(body)
-    });
-
-    res.on("end", function() {
-      if (res.statusCode !== 200) {
-      console.error("Bad status code received: " + res.statusCode + " Message : " + res.statusMessage);
-      } else {
-      console.log("Posting OK");
-      }
+  zlib.gzip(req.body.file, function (err, result) {
+   
+    request({
+      url: 'https://depot.dsnrg.net-entreprises.fr/deposer-dsn/1.0/',
+      method: "POST",
+      gzip: true,
+      headers: {
+        'Authorization': 'DSNLogin jeton='+req.body.jeton,
+        'User-Agent': 'Client-DSN (DsnBuilder/12.5; Paie.fr)',
+        'Content-Type' :'text/plain',
+        'Cache-Control': 'no-cache',
+        //'Content-Type' :'application/json',
+       'Content-Encoding' :'gzip',
+        'Accept-Encoding' :'gzip',
+        //'Accept-Encoding' :'gzip',
+        //'Content-Length' : 4096
+      },
+      body: result
+      }, function (error, response, body){
+        if(!error){
+          console.log(response.statusCode)
+          console.log(response.statusMessage)
+          console.log(zip.decompress(response.body))
+         // res.status(200).send(zip.decompress(response.body));
+         res.status(200).send(response.body);
+        }else{
+          res.status(400).send('error :' + error)
+        }
+  
       });
+
     });
 
-    req2.on("error", function(e) {
-      console.error("Error posting data: " + e.message);
-    });
-
-    req2.write(req.body.file);
-    req2.end();
-
-  // request({
-  //   url: 'https://depot.dsnrg.net-entreprises.fr/deposer-dsn/1.0/',
-  //   method: "POST",
-  //   gzip: true,
-  //   headers: {
-  //     'Authorization': 'DSNLogin jeton='+req.body.jeton,
-  //     'User-Agent': 'Client-DSN (DsnBuilder/12.5; Paie.fr)',
-  //     'Content-Type' :'text/plain',
-  //     'Cache-Control': 'no-cache',
-  //     //'Content-Type' :'application/json',
-  //     'Content-Encoding' :'gzip',
-  //     'Accept-Encoding' :'gzip',
-  //     //'Accept-Encoding' :'gzip',
-  //     //'Content-Length' : 4096
-  //   },
-  //   body: compressed
-  //   }, function (error, response, body){
-  //     if(!error){
-  //       console.log(response.statusCode)
-  //       console.log(response.statusMessage)
-  //       console.log(zip.decompress(response.body))
-  //      // res.status(200).send(zip.decompress(response.body));
-  //      res.status(200).send(response.body);
-  //     }else{
-  //       res.status(400).send('error :' + error)
-  //     }
-
-  //   });
   
 });
 
